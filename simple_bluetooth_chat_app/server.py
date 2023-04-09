d4372e5adc077b930e254e6928ac68e0834d8601
alahirajeffrey <alahirajeffrey@gmail.com>
2023-04-09 22:36:37 +0100

    setup server and client messaging

--- simple_bluetooth_chat_app/server.py
+++ simple_bluetooth_chat_app/server.py
@@ -10,3 +10,18 @@ if __name__ == '__main__':
     server.listen(1)
 
     client, address = server.accept()
+
+    try:
+        while True:
+            data = client.recv(1024)
+            if not data:
+                break
+            print(f"Message: {data.decode('utf-8')}")
+            message= input('Enter message:')
+            client.send(message.encode('utf-8'))
+
+    except OSError as e:
+        pass
+
+    client.clos()
+    server.close()

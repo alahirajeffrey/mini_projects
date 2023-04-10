eda8785431cf1365e8d666b5e30feeb907b07462
alahirajeffrey <alahirajeffrey@gmail.com>
2023-04-10 06:34:47 +0100

    code refactor

--- simple_bluetooth_chat_app/server.py
+++ simple_bluetooth_chat_app/server.py
@@ -1,16 +1,12 @@
 import socket
 
+## mac address of bluetooth server
+BLUETOOH_ADDRESS="58:91:cf:eb:8b:78"
+
 if __name__ == '__main__':
-    
-    ## mac address of bluetooth server
-    BLUETOOH_ADDRESS="58:91:cf:eb:8b:78"
 
     ## setup socket server
-    server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-    
-    ## get bluetooth mac address
-
-    
+    server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)   
     server.bind((BLUETOOH_ADDRESS, 4))
     server.listen(1)
 

2ee6cbeb5cc529393617bb3cfb54a4750c11c76a
alahirajeffrey <alahirajeffrey@gmail.com>
2023-04-10 06:34:03 +0100

    code refactor

--- simple_bluetooth_chat_app/server.py
+++ simple_bluetooth_chat_app/server.py
@@ -2,26 +2,35 @@ import socket
 
 if __name__ == '__main__':
     
+    ## mac address of bluetooth server
     BLUETOOH_ADDRESS="58:91:cf:eb:8b:78"
 
     ## setup socket server
     server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+    
+    ## get bluetooth mac address
+
+    
     server.bind((BLUETOOH_ADDRESS, 4))
     server.listen(1)
 
+    ## accept request from client
     client, address = server.accept()
+    print(f"Connection from {address} has been established")
 
+    ## setup bluetooth messaging
     try:
         while True:
             data = client.recv(1024)
             if not data:
                 break
             print(f"Message: {data.decode('utf-8')}")
-            message= input('Enter message:')
+            message = input('Enter message:')
             client.send(message.encode('utf-8'))
 
-    except OSError as e:
-        pass
+    except socket.error as err:
+        print(err)
 
-    client.clos()
+    ## close client and server connection
+    client.close()
     server.close()

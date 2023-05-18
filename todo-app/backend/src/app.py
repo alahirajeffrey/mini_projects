dd1da37276f837cb211e52db4d132b0c889767ae
alahirajeffrey <alahirajeffrey@gmail.com>
2023-05-18 06:08:03 +0100

    setup routes

--- todo-app/backend/src/app.py
+++ todo-app/backend/src/app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,3 +7,27 @@ app = Flask(__name__)
 def index():
     return 'Welcome'
 
+## create todo
+@app.route('/todo', methods=['POST'] )
+def create_todo():
+    return 'todo created'
+
+## get all todos
+@app.route('/todo', methods=['GET'])
+def get_all_todos():
+    return 'todos'
+
+## get todo by id
+@app.route('/todo/<id>', methods=['GET'])
+def get_todo_by_id(id):
+    return 'todo'
+
+## delete todo by id
+@app.route('/todo/<id>', methods=['DELETE'])
+def delete_todo_by_id(id):
+    return 'todo deleted'
+
+## update todo by id
+@app.route('/todo/<id>', methods=['PATCH'])
+def update_todo(id):
+    return 'todo updated'
